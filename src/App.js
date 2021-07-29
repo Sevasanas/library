0e946d03858530030e00dbd74bf2586f4d829bef
Sevasanas <sevanas86@gmail.com>
2021-07-29 14:05:41 +0300

    created empty array, component Message with fields text and author

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Fragment } from 'react';
 import './App.css';
 import Message from './Components/Message'
-import vinny from './Components/vinny.png'
+
 
 function App() {
+  const [messages, setMessages] = useState([]);
   return (
-    <>
-      <Message text="Давным-давно, кажется, в прошлую пятницу, жил в одной стране медвежонок под именем Винни-Пух. А почему под именем? Потому что над его дверью была надпись: «Винни-Пух», а он под ней жил." />
-      <img src={vinny} alt='vinny' />
-    </>
+   
   );
 }
 
--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import './Message.css'
 
-function Message(props) {
-    return(
-        <div className="message">
-            <h1>Винни Пух</h1>
-            <p>{props.text}</p>
-        </div>
-    );
-}
-
-export default Message
+export const Message = ({ text, author }) => (
+    <div>
+        {author} : {text}
+    </div>
+);
