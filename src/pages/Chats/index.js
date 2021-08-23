71d4ce3c5b00cfeef3d0a2513125ab4dfea73144
Sevasanas <sevanas86@gmail.com>
2021-08-23 15:29:14 +0300

    added sending messages through Realtime Database

--- src/pages/Chats/index.js
+++ src/pages/Chats/index.js
@@ -1,33 +1,85 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import firebase from 'firebase';
 import './home.css';
 import { Form } from '../../Components/Form';
 import { MessageField } from '../../Components/MessageField';
 import { useParams } from 'react-router-dom';
 import { MyList } from '../../Components/MyList';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteChat, sendMessageWithReply } from '../../store/chats/actions';
+import { useSelector } from 'react-redux';
+import { selectName } from '../../store/profile/selectors';
 
 
 export function Chats() {
   
   const { chatId } = useParams();
 
-  /*const [messages, setMessages] = useState([
-    { text: "Hi", author: "Me", id: 1 },
-  ]);*/
+  const [chats, setChats] = useState({});
+  const [messages, setMessages] = useState([]);
 
-  const chats = useSelector(state => state.chats);
-  const dispatch = useDispatch();
+  useEffect(() => {
+    const db = firebase.database();
+    db.ref("chats").on("value", (snapshot) => {
+      let newChats = {};
+      snapshot.forEach((snap) => {
+        const currentChat = snap.val();
+        newChats[currentChat.id] = currentChat;
+      });
 
-  const handleSendMessage = useCallback((newMessage) => {
-      dispatch(sendMessageWithReply(chatId, newMessage));
-  },
-    [chatId]
-  );
-
-  const handleDeleteChat = useCallback((id) => {
-    dispatch(deleteChat(id));
+      setChats(newChats);
+    });
   }, []);
+
+  useEffect(() => {
+    const db = firebase.database();
+    db.ref("messages").on("value", (snapshot) => {
+      let newMessages = {};
+      if (!snapshot) {
+        return;
+      }
+      snapshot.forEach((snap) => {
+        const currentMsgs = snap.val();
+        if (newMessages[snap.key]) {
+          newMessages[snap.key].concat(Object.values(currentMsgs));
+        } else {
+          newMessages[snap.key] = Object.values(currentMsgs);
+        }
+      });
+
+      setMessages(newMessages);
+    });
+
+    return db.ref("messages").off;
+  }, []);
+
+  const addChat = (id, name) => {
+    const db = firebase.database();
+    db.ref("chats").child(id).set({
+      name,
+      id,
+    });
+  };
+
+  const removeChat = (id) => {
+    const db = firebase.database();
+    db.ref("chats").child(id).remove();
+  };
+
+  const name = useSelector(selectName);
+
+  const handleSendMessage = useCallback(
+    (newMessage) => {
+      const db = firebase.database();
+      db.ref("messages")
+        .child(chatId)
+        .push({
+          ...newMessage,
+          author: name,
+          id: `${chatId}-${Date.now()}`,
+          chatId,
+        });
+    },
+    [chatId, name]
+  );
   
   
   return (
@@ -36,12 +88,13 @@ export function Chats() {
         <div>
          <MyList 
          chats={chats} 
-         onDeleteChat={handleDeleteChat}
+         onDeleteChat={removeChat}
+         onAddChat={addChat}
          />
        </div>
        {!!chatId && (
          <div>
-         <MessageField messages={chats[chatId]?.messages} />
+         <MessageField messages={[chatId] || []} />
          <Form onSendMessage={handleSendMessage} />
        </div>
        )}
